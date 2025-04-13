8ec1baa07c789776fd15102996154979a1a0cffd
Renu Kulkarni <rengasings@gmail.com>
2025-04-13 07:39:00 +0000

    Update siteProps with personal information and add launch configuration for VSCode

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Renu Kulkarni",
+  title: "Student",
+  email: "rengasings@gmail.com",
+  gitHub: "renga01",
+  instagram: "",
+  linkedIn: "renu-kulkarni-200324264",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
